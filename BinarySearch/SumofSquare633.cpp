379940628327e0690d209c4c799e44b7930c8327
vishal1202verma <vishalvermao.pcm121@gmail.com>
2024-01-20 12:59:37 +0530

    binary search-2 all leetocde commited

---
+++ BinarySearch/SumofSquare633.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+using namespace std;
+bool isPerfectSquare( int n){
+              int root = sqrt(n);
+              if(root * root == n) return true;
+              else return false;
+    }
+    bool judgeSquareSum (int c) {
+       int x = 0;
+       int y = c;
+       while(x<=y){
+          if(isPerfectSquare(x) && isPerfectSquare(y)){
+              return true;        
+          }
+          else if (!isPerfectSquare(y)){ // y is not perfect sqaure
+              y = (int)sqrt(y) * (int)sqrt(y);
+              x = c-y;
+          }
+          else{ // x is not perfect square
+              x = ((int)sqrt(x) + 1) * ((int)sqrt(x) +1);
+              y = c-x;
+          }
+         
+       }
+        return false;
+    }
+        int main (){
+        int c = 5;
+        bool ans = judgeSquareSum(c);
+        cout<<ans;
+        
+
+
+        }
